c65f62737df4c8d6083758c7138a8fd40f49a78b
VAShaleev <VAShaleev@1cbit.ru>
2019-02-14 14:34:04 +0300

    Add event WorkflowChangedEvent

--- VirtoCommerce.Domain/Order/Events/WorkflowChangedEvent.cs
+++ VirtoCommerce.Domain/Order/Events/WorkflowChangedEvent.cs
@@ -4,9 +4,9 @@ using VirtoCommerce.Domain.Order.Model;
 
 namespace VirtoCommerce.Domain.Order.Events
 {
-    public class WorkflowChangeEvent : GenericChangedEntryEvent<WorkflowModel>
+    public class WorkflowChangedEvent : GenericChangedEntryEvent<WorkflowModel>
     {
-        public WorkflowChangeEvent(IEnumerable<GenericChangedEntry<WorkflowModel>> changedEntries)
+        public WorkflowChangedEvent(IEnumerable<GenericChangedEntry<WorkflowModel>> changedEntries)
            : base(changedEntries)
         {
 
